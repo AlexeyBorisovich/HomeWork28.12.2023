baff337b3bdea34c0cee7d5ff9fb00e5a253730e
AlexeyBorisovich <a.senior@internet.ru>
2023-12-28 00:22:20 +0600

    Решение задачи №3

---
+++ Task003/Task003.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: 
+// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReverse(array, array.Length - 1);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
